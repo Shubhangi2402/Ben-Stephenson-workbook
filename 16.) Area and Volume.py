123a3843e3a10d57075ee5f0b045279edb15bb57
Shubhangi2402 <80590158+Shubhangi2402@users.noreply.github.com>
2021-05-23 10:21:17 +0530

    Add files via upload

---
+++ 16.) Area and Volume.py
@@ -0,0 +1,6 @@
+r = float(input('Enter radius of the circle: '))
+PI = 3.14
+area = float(PI*r*r)
+volume = float(4/3*PI*r*r*r)
+print("The area of the circle is",area)
+print("The volume of the circle is",volume)
